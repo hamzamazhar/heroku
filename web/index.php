459c9219cd05cb2ecdf8c2ffcdd199a0c44eecc9
Hamza Mazhar <hamzamazhar4094@gmail.com>
2017-03-19 00:30:32 +0500

    added db

--- web/index.php
+++ web/index.php
@@ -27,4 +27,13 @@ $app->get('/cowsay', function() use($app) {
   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
 });
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+
 $app->run();
