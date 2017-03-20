e9c5bfb98865114bffce470e967f223261dbbfc4
Hamza Mazhar <hamzamazhar4094@gmail.com>
2017-03-21 04:42:34 +0500

    mongo

--- web/index.php
+++ web/index.php
@@ -2,8 +2,25 @@
 
 require('../vendor/autoload.php');
 
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+
 $client = new MongoDB\Client("mongodb://hamzamazhar4094:sgk=2000@ds137090.mlab.com:37090/database4094");
 
 $companydb = $client->database4094;
 
 $result1 = $companydb->createCollection("employeeCollection");
+
+
+$app->run();
