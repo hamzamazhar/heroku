7e7b0927bcd9ff72c57c50dfc93f72452914617a
Hamza Mazhar <hamzamazhar4094@gmail.com>
2017-03-21 04:51:28 +0500

    mongo

--- web/index.php
+++ web/index.php
@@ -15,19 +15,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
 
 
 $client = new MongoDB\Client("mongodb://hamzamazhar4094:sgk=2000@ds137090.mlab.com:37090/database4094");
 
 $companydb = $client->database4094;
 
-$result1 = $companydb->createCollection("employeeCollection1");
+//$result1 = $companydb->createCollection("employeeCollection1");
 
 
 $app->run();
