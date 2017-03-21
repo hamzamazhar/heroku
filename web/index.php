a9b8d55de9c8846797ea3ed6bc917e0db4576c32
Hamza Mazhar <hamzamazhar4094@gmail.com>
2017-03-21 05:43:08 +0500

    mongo

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require('../vendor/autoload.php');
-require('/views/index.html');
+//require('/views/index.html');
 
 $client = new MongoDB\Client("mongodb://hamzamazhar4094:sgk=2000@ds137090.mlab.com:37090/database4094");
 
